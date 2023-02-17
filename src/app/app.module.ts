59197f6124189caf6d80135bc4180b451347ac0e
Jinal Tandel <jinal.tandel@1rivet.local>
2023-02-17 16:49:04 +0530

    feat: header-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { CoreModule } from './core/core.module';
-
 @NgModule({
   declarations: [
     AppComponent,
