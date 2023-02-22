19f3f4703b3e7d47709a3035ba7bb6869541391a
Jinal Tandel <jinal.tandel@1rivet.local>
2023-02-22 18:20:40 +0530

    feat:sub-header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, NgbModule],
+  imports: [CommonModule, NgbModule, RouterModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
