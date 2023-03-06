df5dc45b37d14939d01cbdc3e4b1a1e075f22d4d
Jinal Tandel <jinal.tandel@1rivet.local>
2023-03-06 13:08:25 +0530

    feat:my-profile-ui

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent } from './profile.component';
+import { ProfileDataContainerComponent } from './profile-data-container/profile-data-container.component';
 
-const routes: Routes = [{ path: '', component: ProfileComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileDataContainerComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ProfileRoutingModule { }
+export class ProfileRoutingModule {}
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -5,17 +5,14 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { ProfileDataContainerComponent } from './profile-data-container/profile-data-container.component';
 import { ProfileDataPresentationComponent } from './profile-data-container/profile-data-presentation/profile-data-presentation.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ProfileComponent,
     ProfileDataContainerComponent,
-    ProfileDataPresentationComponent
+    ProfileDataPresentationComponent,
   ],
-  imports: [
-    CommonModule,
-    ProfileRoutingModule
-  ]
+  imports: [CommonModule, ProfileRoutingModule, ReactiveFormsModule],
 })
-export class ProfileModule { }
+export class ProfileModule {}
